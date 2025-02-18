3ffe492b6dbadbaf10439cac0b9b8be8c2648a97
Marat-Kabirov-334737 <spider.robot.2015@gmail.com>
2025-02-18 20:07:59 +0300

    Добавлена функция group

--- homework02/sudoku.py
+++ homework02/sudoku.py
@@ -41,7 +41,7 @@ def group(values: tp.List[T], n: int) -> tp.List[tp.List[T]]:
     >>> group([1,2,3,4,5,6,7,8,9], 3)
     [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
     """
-    pass
+    return [[values.pop(0) for ind_2 in range(n)] for ind_1 in range(n)]
 
 
 def get_row(grid: tp.List[tp.List[str]], pos: tp.Tuple[int, int]) -> tp.List[str]:
